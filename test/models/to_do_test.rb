505cc502f4a6a6ea6dc8051ae89b409023ce355f
Topstitch <kallen@spoonflower.com>
2015-02-28 16:48:46 -0500

    Basic tests written and passed

--- test/models/to_do_test.rb
+++ test/models/to_do_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class ToDoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "check if to_do has content" do
+    todo = ToDo.new(due_on: 2015-02-27, order: 1, complete: false)
+    assert_not todo.save
+  end
+
+  test "check if to_do has order" do
+    todo = ToDo.new(due_on: 2015-02-27, content: "Buy potatoes", complete: false)
+    assert_not todo.save
+  end
 end
