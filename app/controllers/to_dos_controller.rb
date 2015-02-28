5727546a04080c946b75ef9e77d557507b8bf5d5
Solomon Hilliard <shillia6@eagles.nccu.edu>
2015-02-28 16:07:52 -0500

    Create index page from scratch

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -6,6 +6,7 @@ class ToDosController < ApplicationController
   end
 
   def show
+    redirect_to
   end
 
   def new
@@ -17,7 +18,7 @@ class ToDosController < ApplicationController
 
   def update
     if @todo.update(todo_params)
-      redirect_to @todo, notice: 'ToDo was successfully updated.'
+      redirect_to root_path, notice: 'ToDo was successfully updated.'
     else
       render :edit
     end
@@ -29,7 +30,7 @@ class ToDosController < ApplicationController
   def create
     @todo = ToDo.new(todo_params)
     if @todo.save
-      redirect_to @todo, notice: 'ToDo was successfully created.'
+      redirect_to root_path, notice: 'ToDo was successfully created.'
     else
       render :new
     end
@@ -43,7 +44,7 @@ class ToDosController < ApplicationController
   end
 
   def todo_params
-    params.require(:todo).permit(:content, :due_on, :order, :complete)
+    params.require(:to_do).permit(:content, :due_on, :order, :complete)
   end
 
 end
